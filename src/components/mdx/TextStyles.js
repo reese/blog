78bcf77ff196ca009abfc1e3ca7e14d0f67565ca
Reese Williams <reese@reesew.io>
2020-12-06 01:11:28 -0600

    Remove tooltip for intra-page links.

--- src/components/mdx/TextStyles.js
+++ src/components/mdx/TextStyles.js
@@ -5,6 +5,8 @@ import "tippy.js/dist/tippy.css";
 import "tippy.js/themes/material.css";
 
 export const Anchor = (props) => {
+  if (props.href[0] === "#") return <a {...props} />;
+
   return (
     <Tippy content={props.href} theme="material" animation="scale">
       <a {...props} />
