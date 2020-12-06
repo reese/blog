d475f266444faf32b09fcbdf439724a9a64ad4ef
Reese Williams <reese@reesew.io>
2020-12-06 00:41:13 -0600

    Add tippy popups to article links.

--- src/components/mdx/TextStyles.js
+++ src/components/mdx/TextStyles.js
@@ -1,5 +1,13 @@
+import Tippy from "@tippyjs/react";
 import React from "react";
+import "tippy.js/animations/scale.css";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/themes/material.css";
 
 export const Anchor = (props) => {
-  return <a {...props} />;
+  return (
+    <Tippy content={props.href} theme="material" animation="scale">
+      <a {...props} />
+    </Tippy>
+  );
 };
